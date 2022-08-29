e9b0a9a92c42ddd2ce8ceca302f43e53a547b34d
Evgenii <vorobeievgeniy@gmail.com>
2022-08-29 11:36:33 +0300

    Macrobenchmarks load benchmark data

--- ci/macrobenchmarks.sh
+++ ci/macrobenchmarks.sh
@@ -20,8 +20,16 @@ adb shell pm list instrumentation
 adb shell am instrument -w -e additionalTestOutputDir $BENCHMARK_RESULT_OUTPUT_DEVICE \
                         $PACKAGE_NAME/$BENCHMARK_RUNNER \
                         -e no-isolated-storage true
-## Pull benchmark results
-#adb pull $BENCHMARK_RESULT_OUTPUT_DEVICE/$PACKAGE_NAME-benchmarkData.json \
-#         $BENCHMARK_RESULTS_OUTPUT/$PACKAGE_NAME-test-benchmarkData.json
-#
-#cat $BENCHMARK_RESULTS_OUTPUT/$PACKAGE_NAME-test-benchmarkData.json | wc -l
+# Pull benchmark results
+OUTPUT_JSON_NAME=$PACKAGE_NAME-benchmarkData.json
+adb pull $BENCHMARK_RESULT_OUTPUT_DEVICE/$OUTPUT_JSON_NAME \
+         $BENCHMARK_RESULTS_OUTPUT/$OUTPUT_JSON_NAME
+
+cat $BENCHMARK_RESULTS_OUTPUT/$OUTPUT_JSON_NAME | wc -l
+
+# Check for emulators connected:
+jsonLines=$(cat $BENCHMARK_RESULTS_OUTPUT/$OUTPUT_JSON_NAME | wc -l)
+if [ $jsonLines -lt 1 ]; then
+  echo "\n\u001b[31mERROR: macro benchmark failed\u001b[0m"
+  exit 1
+fi
--- ci/microbenchmarks.sh
+++ ci/microbenchmarks.sh
@@ -8,6 +8,7 @@ PACKAGE_NAME=ru.vorobeij.android.app.benchmark.test
 BENCHMARK_RESULT_OUTPUT_DEVICE=/sdcard/Download
 BENCHMARK_RUNNER=androidx.benchmark.junit4.AndroidBenchmarkRunner
 ROOT=..
+OUTPUT_JSON_NAME=$PACKAGE_NAME-test-benchmarkData.json
 
 cd $ROOT
 # Info: https://developer.android.com/topic/performance/benchmarking/benchmarking-in-ci
@@ -22,5 +23,12 @@ adb shell am instrument -w -e additionalTestOutputDir $BENCHMARK_RESULT_OUTPUT_D
                         -e no-isolated-storage true
 # Pull benchmark results
 adb pull $BENCHMARK_RESULT_OUTPUT_DEVICE/$PACKAGE_NAME-benchmarkData.json \
-         $BENCHMARK_RESULTS_OUTPUT/$PACKAGE_NAME-test-benchmarkData.json
-cat $BENCHMARK_RESULTS_OUTPUT/$PACKAGE_NAME-test-benchmarkData.json | wc -l
+         $BENCHMARK_RESULTS_OUTPUT/$OUTPUT_JSON_NAME
+cat $BENCHMARK_RESULTS_OUTPUT/$OUTPUT_JSON_NAME | wc -l
+
+# Check for emulators connected:
+jsonLines=$(cat $BENCHMARK_RESULTS_OUTPUT/$OUTPUT_JSON_NAME | wc -l)
+if [ $jsonLines -lt 1 ]; then
+  echo "\n\u001b[31mERROR: benchmark failed\u001b[0m"
+  exit 1
+fi
