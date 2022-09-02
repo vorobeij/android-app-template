4791de6d660f262af672691638177502654d188b
Evgenii <vorobeievgeniy@gmail.com>
2022-09-02 17:29:56 +0300

    Fix benchmarks analytics

--- ci/analyse_benchmarks.sh
+++ ci/analyse_benchmarks.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 # TODO remove me
-#cp /Users/sj/.m2/repository/ru/vorobeij/benchmark/regress/1.0/*.jar ./regress.jar
+cp /Users/sj/.m2/repository/ru/vorobeij/benchmark/regress/1.0/*.jar ./regress.jar
 
-STORAGE_FILE=../benchmarks/reports/benchmarks.json
-java -jar regress.jar --projectRootPath $(readlink -f ../) \
-                      --threshold 10 \
-                      --storageFilePath $(readlink -f $STORAGE_FILE)
+STORAGE_FILE=`pwd`/../benchmarks/reports/benchmarks.json
+java -jar regress.jar --projectRootPath `pwd`/.. --threshold 10 --storageFilePath /Users/sj/AndroidApps/AndroidAppTemplate/benchmarks/reports/benchmarks.json
