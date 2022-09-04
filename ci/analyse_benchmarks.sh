4fd4824309f068cf5c3896d15207d89b8a5781e2
Evgenii <vorobeievgeniy@gmail.com>
2022-09-04 19:16:16 +0300

    Refactoring

--- ci/analyse_benchmarks.sh
+++ ci/analyse_benchmarks.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # TODO remove me
-cp /Users/sj/.m2/repository/ru/vorobeij/benchmark/regress/1.0/*.jar ./regress.jar
+#cp /Users/sj/.m2/repository/ru/vorobeij/benchmark/regress/1.0/*.jar ./regress.jar
 
 STORAGE_FILE=`pwd`/../benchmarks/reports/benchmarks.json
 java -jar regress.jar --projectRootPath `pwd`/.. --threshold 10 --storageFilePath /Users/sj/AndroidApps/AndroidAppTemplate/benchmarks/reports/benchmarks.json
